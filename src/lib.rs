26afd5e6d86fb2e79c593a853da5f6f1bf57f76e
magwoo <magwood51@yandex.ru>
2025-05-28 14:44:27 +0300

    fix work stealing

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,11 @@
 use core::future::Future;
 use crossbeam_deque::Injector as SharedQueue;
-use crossbeam_deque::Steal;
-use crossbeam_deque::Stealer;
 use crossbeam_deque::Worker as WorkerQueue;
+use crossbeam_deque::{Steal, Stealer};
 use std::pin::Pin;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::sync::OnceLock;
-use std::task::Context;
-use std::task::Poll;
-use std::task::Waker;
-use std::time::Duration;
-use std::time::Instant;
+use std::sync::{Arc, Mutex, OnceLock};
+use std::task::{Context, Poll, Waker};
+use std::time::{Duration, Instant};
 
 pub use handle::JoinHandle;
 
@@ -36,7 +30,7 @@ pub fn create(workers_num: usize) {
     SHARED_QUEUE.get_or_init(SharedQueue::default);
 
     let workers_queues = (0..workers_num)
-        .map(|_| WorkerQueue::new_lifo())
+        .map(|_| WorkerQueue::new_fifo())
         .collect::<Vec<_>>();
 
     let stealers = workers_queues
@@ -80,20 +74,26 @@ pub fn block_on(future: impl Future<Output = ()> + 'static + Send) {
 }
 
 impl Worker {
-    const MIN_PASS_TIMEOUT: Duration = Duration::from_micros(5);
+    const MIN_PASS_TIMEOUT: Duration = Duration::from_micros(50);
     const MAX_PASS_TIMEOUT: Duration = Duration::from_millis(1);
 
-    fn event_loop(self) {
+    fn event_loop(self) -> ! {
         let mut pass_timeout = Self::MIN_PASS_TIMEOUT;
         let mut last_pass = Instant::now();
 
         loop {
             let mut solved = 0;
-            while let Some(task) = self.search_task() {
-                task.run(&self.local_queue);
+            let mut pending_tasks = Vec::new();
+
+            while let Some(task) = self.search_tasks() {
+                task.run(&mut pending_tasks);
 
                 solved += 1;
             }
+
+            pending_tasks.into_iter().for_each(|task| {
+                self.local_queue.push(task);
+            });
 
             pass_timeout = match solved {
                 1.. => Self::MIN_PASS_TIMEOUT,
@@ -110,17 +110,17 @@ impl Worker {
         }
     }
 
-    fn search_task(&self) -> Option<Task> {
+    fn search_tasks(&self) -> Option<Task> {
+        let _ = SHARED_QUEUE
+            .get()
+            .expect("Runtime is not created")
+            .steal_batch(&self.local_queue);
+
         self.local_queue.pop().or_else(|| {
-            let shared_queue = SHARED_QUEUE.get().expect("Runtime is not created");
-            if let Steal::Success(task) = shared_queue.steal() {
-                Some(task)
-            } else {
-                self.stealers.iter().find_map(|s| match s.steal() {
-                    Steal::Success(task) => Some(task),
-                    _ => None,
-                })
-            }
+            self.stealers.iter().find_map(|s| match s.steal() {
+                Steal::Success(task) => Some(task),
+                _ => None,
+            })
         })
     }
 }
@@ -132,13 +132,13 @@ impl Task {
         }
     }
 
-    fn run(mut self, local_queue: &WorkerQueue<Self>) {
+    fn run(mut self, pendings: &mut Vec<Self>) {
         let waker = Waker::noop();
 
         // FIXME: move result return here
         match self.future.as_mut().poll(&mut Context::from_waker(waker)) {
             Poll::Ready(_) => println!("some task is done"),
-            Poll::Pending => local_queue.push(self),
+            Poll::Pending => pendings.push(self),
         }
     }
 }
